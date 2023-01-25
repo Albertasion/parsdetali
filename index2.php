7ba185c2c5782151d2c1cd598af4acc2e7fc4df3
Albertasion <93448198+Albertasion@users.noreply.github.com>
2023-01-26 00:59:45 +0200

    Create index2.php

---
+++ index2.php
@@ -0,0 +1,151 @@
+<?php
+include_once 'functions.php';
+require 'phpQuery.php';
+require 'vendor/autoload.php';
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+
+//отримуємо всі головні посилання
+$request = requests('https://detali.org.ua/uk/');
+$output = phpQuery::newDocument($request);
+$menu = $output->find('.cbp-vertical-on-top li a');
+foreach ($menu as $key => $value){
+  $pq = pq($value);
+  $src_menu[$key]["href"] = $pq->attr("href");
+  $src_menu[$key]['text_link'] = $pq->text();
+}
+//перевіряемо уникальність посилань
+$src_menu_formated = array_diff($src_menu, array('', NULL, false));
+
+echo "Табиця заполнена";
+// format($src_menu_formated);
+
+
+$servername = "localhost";
+$database = "detali";
+$username = "parseradmin";
+$password = "1234";
+//Создаем соединение
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Проверяем соединение
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfull". " ". $database. "<br>";
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+format($src_menu_formated);
+//CREATE USER 'parseradmin'@'localhost' IDENTIFIED BY '1234';
+//GRANT ALL PRIVILEGES ON detali.* TO 'parseradmin'@'localhost';
+//CREATE TABLE `detali`.`detalimenu` ( `id` INT NOT NULL AUTO_INCREMENT , `menulinks` TEXT NOT NULL , 'menulinkstext` TEXT NOT NULL, PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+//Заносимо в базу основни посилання
+foreach ($src_menu_formated as $key) {
+  echo 'key-----'.$key . '<br>';
+  foreach ($key as $v1=>$v2) {
+    echo 'v1-----'. $v1.'<br>';
+    echo 'v2-----'. $v2.'<br>';
+    
+}
+  format($key);
+//     $query = "INSERT INTO detalimenu (id, menulinks) VALUES (NULL, '$link')";
+//     $result = mysq li_query($conn, $query);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//отримуємо головні сабменю
+// foreach ($src_menu_formated as $value) {
+  // if ($value == 'https://detali.org.ua/uk/102-zapchasti-dlya-betonomeschalki') {
+  //   break;
+  // }
+//   $request_menus = requests($value);
+//   $output = phpQuery::newDocument($request_menus);
+//   $submenu = $output->find('.subcategory-image a');
+//   foreach ($submenu as $key =>$value){
+//   $pq = pq($value);
+//   $href = $pq->attr("href");
+//   $src_submenu[] = $href;
+//     echo $src_menu[$key]. '<br>';
+// }
+// }
+// $src_menu_unique = array_unique($src_submenu);
+// format($src_menu_unique);
+// echo count($src_submenu);
+// echo count($src_menu_unique);
+
+
+
+// //отримуємо сабменю3
+// foreach ($src_submenu as $value) {
+  // if ($value == 'https://detali.org.ua/uk/84-zapchasti-dlya-benzopil') {
+  //   break;
+  // }
+//   $request_menus1 = requests($value);
+//   $output1 = phpQuery::newDocument($request_menus1);
+//   $submenu1 = $output1->find('.subcategory-image a');
+//   foreach ($submenu1 as $key =>$value){
+//   $pq = pq($value);
+//   $src_submenu_category[] = $pq->attr("href");
+// }
+// }
+// echo "Сабменю заповнено";
+
+
+
+// //отримуємо сабменю4
+// foreach ($src_submenu_category as $value) {
+// $request_menus2 = requests($value);
+// $output2 = phpQuery::newDocument($request_menus2);
+// $submenu2 = $output2->find('.product-name-container a');
+//   foreach ($submenu2 as $key =>$value){
+//   $pq = pq($value);
+//   $src_submenu_category_last[] = $pq->attr("href");
+// }
+// }
+// echo "Сабменю последнее заповнено";
+
+
+
+
+
+// $spreadsheet = new Spreadsheet();
+// $sheet = $spreadsheet->getActiveSheet();
+// foreach ($src_menu_formated as $key => $value) {
+//   $sheet->setCellValue('A'. $key, $value);
+//   }
+// foreach ($src_submenu as $key => $value) {
+// $sheet->setCellValue('B'. $key, $value);
+// }
+
+// foreach ($src_submenu_category as $key => $value) {
+//   $sheet->setCellValue('C'. $key, $value);
+//   }
+
+//   foreach ($src_submenu_category_last as $key => $value) {
+//     $sheet->setCellValue('D'. $key, $value);
+//     }
+
+
+// $writer = new Xlsx($spreadsheet);
+// $writer->save('Links.xlsx');
